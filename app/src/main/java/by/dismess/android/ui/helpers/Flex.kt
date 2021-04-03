5a752ad7649210e8a0cacdd709da05726dc06642
VKuzia <v.kuzia.mail@gmail.com>
2021-04-04 02:48:35 +0300

    Introduce MutableStateList MutableStateList is now created from incoming messages list and is wisely forwarded to components

--- app/src/main/java/by/dismess/android/ui/helpers/Flex.kt
+++ app/src/main/java/by/dismess/android/ui/helpers/Flex.kt
@@ -1,12 +1,12 @@
-package by.dismess.android.ui
+package by.dismess.android.ui.helpers
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+// import androidx.compose.foundation.layout.Box
+// import androidx.compose.runtime.Composable
+// import androidx.compose.ui.Modifier
 
-@Composable
-fun FlexChild(modifier: Modifier, content: @Composable () -> Unit) {
-    Box(modifier = modifier) {
-        content()
-    }
-}
+// @Composable
+// fun FlexChild(modifier: Modifier, content: @Composable ) {
+//    Box(modifier = modifier) {
+//
+//    }
+// }
