39d936adfbb39d13efee0859a10c1667ecad505c
VKuzia <v.kuzia.mail@gmail.com>
2021-04-04 04:28:32 +0300

    Add interactive scrolling Now LazyColumn scrolls down any time message is sent

--- app/src/main/java/by/dismess/android/frames/DialogFrame.kt
+++ app/src/main/java/by/dismess/android/frames/DialogFrame.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
@@ -15,21 +16,31 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import by.dismess.android.forms.MessageForm
 import by.dismess.android.forms.MessageType
+import kotlinx.coroutines.launch
 import androidx.compose.runtime.Composable as Composable
 
 @Composable
 fun DialogFrameImpl(chatName: String, messages: MutableList<String>, onBackToChats: () -> Unit) {
     val messagesList = remember { messages.toMutableStateList() }
+    val lazyListState = remember { LazyListState() }
+    val coroutineScope = rememberCoroutineScope()
+
     Column {
         TopPanel(chatName, onBackToChats)
-        MessageList(Modifier.weight(10f), messagesList)
-        TextPanel(Modifier.weight(1f)) { messagesList.add(it) }
+        MessageList(Modifier.weight(10f), lazyListState, messagesList)
+        TextPanel(Modifier.weight(1f)) {
+            messagesList.add(it)
+            coroutineScope.launch {
+                lazyListState.animateScrollToItem(messagesList.lastIndex)
+            }
+        }
     }
 }
 
@@ -46,8 +57,8 @@ fun TopPanel(chatName: String, onBackToChats: () -> Unit) {
 }
 
 @Composable
-fun MessageList(modifier: Modifier, messages: SnapshotStateList<String>) {
-    LazyColumn(modifier = modifier) {
+fun MessageList(modifier: Modifier, state: LazyListState, messages: SnapshotStateList<String>) {
+    LazyColumn(modifier = modifier, state = state) {
         items(messages) {
             MessageForm(it, "3:45", MessageType.OWNERS)
             MessageForm(it, "3:45", MessageType.OTHERS)
