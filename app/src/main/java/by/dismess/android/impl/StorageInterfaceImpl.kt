a9fcde256880ccf816740c6941f52386ea2a47b6
Derovi <14347002+Derovi@users.noreply.github.com>
2021-04-25 17:48:29 +0300

    Core integration, StorageInterface impl (#3)
    * Added Core dependency
    
    * Added Snappy key-value storage dependency
    
    * Koin integration
    
    * StorageInterface implementation with Snappy
    
    * Updated core version, StorageInterface ::exists, ::forget impl
    
    * Fixed ktlint.yml
    
    * Fixed ktlint.yml
    
    * Updated ktlint to v2
    
    * Fixed codestyle
    
    * Changed codestyle
    
    * Fixed codestyle

---
+++ app/src/main/java/by/dismess/android/impl/StorageInterfaceImpl.kt
@@ -0,0 +1,16 @@
+package by.dismess.android.impl
+
+import by.dismess.core.outer.StorageInterface
+import com.snappydb.DB
+
+class StorageInterfaceImpl(
+    private val snappyDB: DB
+) : StorageInterface {
+    override suspend fun exists(key: String): Boolean = snappyDB.exists(key)
+
+    override suspend fun forget(key: String): Unit = snappyDB.del(key)
+
+    override suspend fun loadRawData(key: String): ByteArray = snappyDB.getBytes(key)
+
+    override suspend fun saveRawData(key: String, data: ByteArray): Unit = snappyDB.put(key, data)
+}
