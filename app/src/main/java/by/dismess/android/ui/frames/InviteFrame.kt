60a88aefbcacb048e645ca774b9a07543105cb7f
VKuzia <v.kuzia.mail@gmail.com>
2021-04-29 13:53:41 +0300

    Beautify InviteFrame Added onDone actions for textfields, decorated text and spacing for a more pleasant view

--- app/src/main/java/by/dismess/android/ui/frames/InviteFrame.kt
+++ app/src/main/java/by/dismess/android/ui/frames/InviteFrame.kt
@@ -1,11 +1,16 @@
 package by.dismess.android.ui.frames
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -13,39 +18,90 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import by.dismess.android.ui.theming.theme.BackgroundColor
 import by.dismess.android.ui.theming.theme.DismessTheme
+import by.dismess.android.ui.theming.theme.OrangePrimary
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+private const val someValidationError = "Validation Error :(\nTry again please" // Demo only
 
 @Composable
 fun InviteFrameImpl(validate: (String, String) -> Boolean, onValidInvite: () -> Unit) {
     val inviteFieldState = remember { mutableStateOf(TextFieldValue()) }
     val loginFieldState = remember { mutableStateOf(TextFieldValue()) }
+    val runningValidationState = remember { mutableStateOf(false) }
+    val errorMessageState = remember { mutableStateOf<String?>(null) }
+    val coroutineScope = rememberCoroutineScope()
+
     Column(
         modifier = Modifier
-            .fillMaxWidth().fillMaxHeight(),
+            .fillMaxWidth()
+            .fillMaxHeight(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Greet()
-        Text("Enter service invite")
-        InviteField(inviteFieldState)
-        Text("Enter your login")
+        Box(
+            modifier = Modifier
+                .fillMaxHeight(0.2f)
+                .wrapContentSize()
+        ) {
+            StatusPanel(runningValidationState, errorMessageState)
+        }
         LoginField(loginFieldState)
+        InviteField(inviteFieldState)
         Button(
             onClick = {
-                if (validate(inviteFieldState.value.text, loginFieldState.value.text)) {
-                    onValidInvite()
+                coroutineScope.launch {
+                    runningValidationState.value = true
+                    var validationResult =
+                        validate(inviteFieldState.value.text, loginFieldState.value.text)
+                    // These lines are for demo only. We emulate hard work of validation.
+                    delay(2000)
+                    if (Random.nextBoolean()) {
+                        validationResult = false
+                    }
+                    runningValidationState.value = false
+                    if (validationResult) {
+                        errorMessageState.value = null
+                        onValidInvite()
+                    } else {
+                        errorMessageState.value = someValidationError
+                    }
                 }
             }
         ) {
             Text("Proceed")
         }
-        Spacer(modifier = Modifier.fillMaxHeight(0.4f))
+        Spacer(modifier = Modifier.fillMaxHeight(0.2f))
+    }
+}
+
+@Composable
+fun StatusPanel(
+    runningValidationState: MutableState<Boolean>,
+    errorMessageState: MutableState<String?>
+) {
+    if (runningValidationState.value) {
+        CircularProgressIndicator()
+    } else {
+        if (errorMessageState.value != null) {
+            Text(
+                text = someValidationError,
+                color = MaterialTheme.colors.error,
+                style = MaterialTheme.typography.h5
+            )
+        }
     }
 }
 
@@ -53,23 +109,37 @@ fun InviteFrameImpl(validate: (String, String) -> Boolean, onValidInvite: () -> 
 private fun Greet() {
     Text(
         "Welcome to Dismess",
-        textAlign = TextAlign.Center
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.h2
     )
-}
-
-@Composable
-private fun InviteField(fieldState: MutableState<TextFieldValue>) {
-    TextField(
-        value = fieldState.value,
-        onValueChange = { fieldState.value = it }
+    Text(
+        "Best distributed messenger of all time",
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.subtitle1,
+        color = OrangePrimary,
+        textDecoration = TextDecoration.LineThrough,
     )
 }
 
 @Composable
 private fun LoginField(fieldState: MutableState<TextFieldValue>) {
+    CustomTextField(fieldState = fieldState, labelText = "Enter your login")
+}
+
+@Composable
+private fun InviteField(fieldState: MutableState<TextFieldValue>) {
+    CustomTextField(fieldState = fieldState, labelText = "Enter service invite")
+}
+
+@Composable
+private fun CustomTextField(fieldState: MutableState<TextFieldValue>, labelText: String) {
+    val localFocusManager = LocalFocusManager.current
     TextField(
         value = fieldState.value,
-        onValueChange = { fieldState.value = it }
+        onValueChange = { fieldState.value = it },
+        label = { Text(labelText) },
+        singleLine = true,
+        keyboardActions = KeyboardActions(onDone = { localFocusManager.clearFocus() }),
     )
 }
 
