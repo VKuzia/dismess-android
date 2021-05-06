a4850847fe2ac115eac0a73961057234b8f4efc3
VKuzia <v.kuzia.mail@gmail.com>
2021-05-06 12:55:49 +0300

    Implement basic user addition

--- app/src/main/java/by/dismess/android/ui/controllers/FindUserFrameController.kt
+++ app/src/main/java/by/dismess/android/ui/controllers/FindUserFrameController.kt
@@ -1,11 +1,14 @@
 package by.dismess.android.ui.controllers
 
+import by.dismess.android.service.DemoStorage
 import by.dismess.android.service.model.User
 import by.dismess.android.ui.controllers.interfaces.FindUserFrameInterface
 import by.dismess.core.model.UserID
+import org.koin.core.context.GlobalContext.get
 import java.math.BigInteger
 
-class FindUserFrameController : FindUserFrameInterface {
+class FindUserFrameController(private val storage: DemoStorage = get().get()) :
+    FindUserFrameInterface {
     override fun refreshHistory() {
 //        TODO("Not yet implemented")
     }
@@ -15,6 +18,6 @@ class FindUserFrameController : FindUserFrameInterface {
     }
 
     override fun addUser(user: User) {
-        //
+        storage.addChat(user)
     }
 }
